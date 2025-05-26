c6bec48968b3a4007900c40aedf5567b66dd64fb
skuerjuan <maierowiczfelipe@gmail.com>
2025-05-26 10:19:39 -0300

    Score en pantalla

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public UIManager uiManager;
+    void Start()
+    {
+        uiManager = FindObjectOfType <UIManager>();
+        score = 0;
+    }
+
+    public void AddScore()
+    {
+        score++;
+        uiManager.UpdateScore(score);
+    }
+}
