af2c2778ffcda88037665a8dbf0cff575ee74ad8
skuerjuan <maierowiczfelipe@gmail.com>
2025-06-09 10:34:25 -0300

    execution order scene

---
+++ Assets/Scripts/ExecutionOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExecutionOrder : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("awake");
+    }
+    private void Start()
+    {
+        Debug.Log("start");
+    }
+    private void Update()
+    {
+        Debug.Log("update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("fixed update");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("onEnable");
+    }
+}
---
+++ Assets/Scripts/WaitNSeconds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitNSeconds : MonoBehaviour
+{
+    public float timeToWait;
+    float timeElapsed;
+    public Transform cuboTR;
+    void Start()
+    {
+        //InvokeRepeating(nameof(ElevateCube), 0, timeToWait);
+        StartCoroutine(WaitUntilElevate());
+    }
+
+    void Update()
+    {
+        //if(timeElapsed >= timeToWait)
+        //{
+        //    Debug.Log("tiempo cumplido");
+        //    ElevateCube();
+        //    timeElapsed = 0;
+        //}
+        //else
+        //{
+        //    Debug.Log(timeElapsed += Time.deltaTime);
+        //}
+    }
+
+    IEnumerator WaitUntilElevate()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeToWait);
+            ElevateCube();
+        }
+    }
+
+    void ElevateCube()
+    {
+        cuboTR.position += Vector3.up * 2;
+    }
+}
