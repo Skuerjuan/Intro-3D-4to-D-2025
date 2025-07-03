271392fa4bfc29c41478f32d08ad9d419d22e338
skuerjuan <maierowiczfelipe@gmail.com>
2025-07-03 10:41:34 -0300

    interaction

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public GameObject UIInteractionMessage;
+    public bool canInteract;
+    public MercaderiaScript mercaderia;
+
+    void Start(){
+        UIInteractionMessage.SetActive(false);
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.E) && canInteract){
+            Destroy(mercaderia.gameObject);
+            EndInteraction();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log(other.gameObject.name);
+        mercaderia = other.GetComponent<MercaderiaScript>();
+        if(mercaderia){
+            UIInteractionMessage.SetActive(true);
+            canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        EndInteraction();
+    }
+
+    void EndInteraction(){
+        UIInteractionMessage.SetActive(false);
+        mercaderia = null;
+        canInteract = false;
+    }
+}
